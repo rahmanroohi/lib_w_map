c86e58d54b9b54e2934d8475f5db8d28d289feaf
rahmanroohi <rahmanroohi@gmail.com>
2016-12-14 16:40:47 +0330

    change android

--- src/android/view/ext/ArrayDegreeProvider.java
+++ src/android/view/ext/ArrayDegreeProvider.java
@@ -10,14 +10,15 @@ package android.view.ext;
 public class ArrayDegreeProvider implements IDegreeProvider {
 	private float[] degrees;
 	
-	public ArrayDegreeProvider(float[] degrees) {
-		this.degrees = degrees;
-	}
-	
 	public float[] getDegrees(int count, float totalDegrees){
 		if(degrees == null || degrees.length != count){
             throw new IllegalArgumentException("Provided delta degrees and the action count are not the same.");
         }
 		return degrees; 
 	}
+	public ArrayDegreeProvider(float[] degrees) {
+		this.degrees = degrees;
+	}
+	
+	
 }
